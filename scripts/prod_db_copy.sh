b847af44dd231d75d588f958b68c8a17bff764d9
slgy <me@nowhere.com>
2025-02-21 08:22:30 -0500

    updated

--- scripts/prod_db_copy.sh
+++
@@ -1,3 +0,0 @@
-curl https://world.stevenamoore.dev/api/admin/dbdump
-cp -f /root/prod/world-prod/api/dbbackups/* /root/dev/world-dev/api/dbbackups/
-curl https://dev.world.stevenamoore.dev/api/admin/dbload
--- tasks/tasks.py
+++ tasks/tasks.py
@@ -6,25 +6,17 @@ from autonomous.tasks import AutoTasks
 from models.user import User
 
 
-
 ####################################################################################################
 # Tasks
 ####################################################################################################
-def _generate_task(model, pk):
-    if Model := World.get_model(model):
-        obj = Model.get(pk)
-        obj.generate()
-        if not obj.image:
-            AutoTasks().task(
-                _generate_image_task,
-                model,
-                pk,
-            )
-    return {"url": f"/api/manage/{obj.path}"}
-
-def _generate_image_task(model, pk):
-    if Model := World.get_model(model):
-        obj = Model.get(pk)
-        obj.resummarize()
-        obj.generate_image()
-    return {"url": f"/api/{obj.path}/details"}
+# def _generate_task(model, pk):
+#     if Model := AutoModel.get_model(model):
+#         obj = Model.get(pk)
+#         obj.generate()
+#         if not obj.image:
+#             AutoTasks().task(
+#                 _generate_image_task,
+#                 model,
+#                 pk,
+#             )
+#     return {"url": f"/api/manage/{obj.path}"}
